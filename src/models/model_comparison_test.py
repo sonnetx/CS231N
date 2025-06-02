1771eca6a5f7f5afe3682ede3b8441eae2a56fe9
EC2 Default User <ec2-user@ip-172-31-41-80.ec2.internal>
2025-06-02 21:22:02 +0000

    DINO finetuning

--- src/models/model_comparison_test.py
+++ src/models/model_comparison_test.py
@@ -140,17 +140,17 @@ def main(num_train_images=25000, proportion_per_transform=0.2, resolution=224):
     }
 
     models = [
-        {"name": "vit", "model_id": "google/vit-base-patch16-224", "type": "vit", "config": {
-            "image_size": resolution,
-            "patch_size": 16,
-            "num_labels": NUM_FILTERED_CLASSES,
-            "ignore_mismatched_sizes": True
-        }},
-        # {"name": "dinov2", "model_id": "facebook/dinov2-base", "type": "dinov2", "config": {
+        # {"name": "vit", "model_id": "google/vit-base-patch16-224", "type": "vit", "config": {
         #     "image_size": resolution,
+        #     "patch_size": 16,
         #     "num_labels": NUM_FILTERED_CLASSES,
         #     "ignore_mismatched_sizes": True
         # }},
+        {"name": "dinov2", "model_id": "facebook/dinov2-base", "type": "dinov2", "config": {
+            "image_size": resolution,
+            "num_labels": NUM_FILTERED_CLASSES,
+            "ignore_mismatched_sizes": True
+        }},
         # {"name": "simclr", "model_id": "resnet50", "type": "simclr", "config": {
         #     "img_size": resolution
         # }},
